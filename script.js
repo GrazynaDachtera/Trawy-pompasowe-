6ec26fdf08892141b9261c199cc42dc512ae9181
Grażyna Dachtera <grazynadachtera@gmail.com>
2023-03-27 21:04:30 +0100

    added changes in index.html, styles.css, script.js

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".slider img");
+
+let counter = 0;
+const size = images[0].clientWidth;
+
+slider.style.transform = "translateX(" + (-size * counter) + "px)";
+
+setInterval(() => {
+    if (counter < images.length - 1) {
+        counter++;
+    } else {
+        counter = 0;
+    }
+    slider.style.transform = "translateX(" + (-size * counter) + "px)";
+}, 4000);
+
+
+
